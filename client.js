f19d50e5e32c84be3b5b257b7880530a619a583e
mclmnop <m_charlebois@hotmail.com>
2021-02-10 13:16:44 -0500

    can quit but cant play

--- client.js
+++ client.js
@@ -6,14 +6,29 @@ Establishes connection with the game server
 
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
+    host: '135.23.222.131',
+    port: 50542
+    }, () => {
+      console.log('Successfully connected to server!')
+    });
   //interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('data', (data) => {
     console.log('Server message: ', data);
+    
+/*      */
   });
+  const name = "Name: MC";
+  conn.write(name);
+/*   conn.write("Move: up");
+  conn.write("Move: right");
+  conn.write("Move: down");
+  conn.write("Move: left"); */
+
+
+
+
+
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,6 +1,25 @@
+const net = require('net');
 const { connect } = require('./client');
 console.log('Connecting...');
 connect();
 
+const handleUserInput = function(input) {
+  if (input === '\u0003') {
+    process.exit();
+  } else {
+    return DataCue;
+  }
+}
 
+const setupInput = function(callback) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (input) => {
+    callback(input);
+  })
+  return stdin;
+}
 
+setupInput(handleUserInput)
