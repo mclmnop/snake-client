455b92803c78d667281cb0735e72a754a7a8608a
mclmnop <m_charlebois@hotmail.com>
2021-02-10 14:02:22 -0500

    snake can move

--- input.js
+++ input.js
@@ -1,20 +1,41 @@
-const handleUserInput = function(input) {
-  if (input === '\u0003') {
+let connection;
+
+const handleUserInput = function(input, conn) {
+  //ctrl+c =quit
+   if (input === '\u0003') {
     process.exit();
-  } else {
-    return DataCue;
+  } 
+  //w = move up
+  if (input === '\u0077') {
+    conn.write("Move: up");
   }
+  //a = move left
+  if (input === '\u0061') { 
+    conn.write("Move: left");
+  }
+  //s = move down
+  if (input === '\u0073') {
+    conn.write("Move: down");
+  }
+  //d = move right
+  if (input === '\u0064') {
+    conn.write("Move: right");
+  }
+
 }
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (input) => {
-    handleUserInput(input);
+    handleUserInput(input, conn);
   })
   return stdin;
 }
+
+
 
 module.exports = {setupInput};
